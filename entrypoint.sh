fae72abfe17bf21033be8ab833c1d0cf9b337061
yellowmegaman <yellowmegaman@gmail.com>
2020-03-02 14:37:46 +0300

    user/email settings for commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
   exit 1
 fi
 
+git config --global user.name "$INPUT_GIT_USER"
+git config --global user.email "$INPUT_GIT_EMAIL"
+
 for branch in $(curl -s -H "authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -X GET "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls" | jq -r '.[].head.ref'); do
 	git clone "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" -b "$branch" "$branch"
 	pushd "$branch"
